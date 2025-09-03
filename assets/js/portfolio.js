cc4b4aa856424ec19ec504e3dace7366062d7b3f
alif <aliffal123dj@gmail.com>
2025-09-04 07:46:03 +0800

    kopiga update design

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -8,8 +8,8 @@ new Chart(ctx, {
     datasets: [{
       label: 'Pertumbuhan Kopiga',
       data: [10, 35, 60, 120, 200],
-      borderColor: '#6d4c41',
-      backgroundColor: 'rgba(109, 76, 65, 0.3)',
+      borderColor: '#3c2a21',
+      backgroundColor: 'rgba(60, 42, 33, 0.3)',
       tension: 0.4,
       fill: true,
       pointRadius: 6,
